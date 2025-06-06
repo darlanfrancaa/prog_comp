de6a7a705a6221c8e9dae3c43f7cce9e8ab4f587
Darlan <darlanvaf12@gmail.com>
2025-06-05 22:28:09 -0300

    new problem

--- usaco_guide/tree_algs/mootube.cpp
+++ usaco_guide/tree_algs/mootube.cpp
@@ -3,10 +3,45 @@ using namespace std;
 
 typedef long long ll;
 const int MOD = 1e9 + 7;
+using pii = pair<int,int>;
+int n;
+vector<vector<pii>> adj;
+vector<bool> visitado;
+int contador, k;
+ 
+void dfs(int v){
+    visitado[v] = true;
+    for(auto [node, relevancia]: adj[v]){
+        if(!visitado[node] && relevancia >= k){
+            contador++;
+            dfs(node);
+        }
+    }
+}
 
 int main() {
-    //freopen("mootube.in", "r", stdin);
-    //freopen("mootube.out", "w", stdout);
+    freopen("mootube.in", "r", stdin);
+    freopen("mootube.out", "w", stdout);
     ios::sync_with_stdio(0); cin.tie(0);
+    int q; cin >> n >> q;
+    adj.resize(n,vector<pii>());
+    for(int i=0;i<n-1;i++){
+        int a, b , relevancia; 
+        cin >> a >> b >> relevancia;
+        a--; b--;
+        adj[a].push_back({b,relevancia});
+        adj[b].push_back({a,relevancia});
+    }
+    
+    for(int i=0;i<q;i++){
+        int v; 
+        cin >> k >> v;
+        v--;
+        visitado = vector<bool>(n);
+        contador = 0;
+        dfs(v);
+        cout << contador << endl;
+    }
+
     return 0;
 }
