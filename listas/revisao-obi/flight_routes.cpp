27842dcff2a09b57861c120fd954aacb2f79a911
DarlanFranca123 <darlanvaf12@gmail.com>
2025-09-07 18:56:19 -0300

    new problem

---
+++ listas/revisao-obi/flight_routes.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+using pii = pair<int,int>;
+const int MOD = 1e9 + 7;
+
+//link : https://cses.fi/problemset/task/1682/
+
+void bfs(const vector<vector<int>> adj, int ini, vector<bool> &visited){
+    queue<int> q; 
+    q.push(ini);
+    while(!q.empty()){
+        int v = q.front(); q.pop();
+        visited[v] = true;
+        for(auto &u: adj[v]){
+            if(!visited[u]) q.push(u);
+        }   
+    }
+}
+
+
+
+signed main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n,m; cin >> n >> m; 
+    vector<vector<int>> adj1(n,vector<int>()), adj2(n,vector<int>());
+    for(int i=0;i<m;i++){
+        int a,b; cin >> a >> b; 
+        a--, b--; 
+        adj1[a].push_back(b);
+        adj2[b].push_back(a);
+    }
+    vector<bool> visited(n, false);
+    bfs(adj1, 0, visited);
+    for(int i=0;i<n;i++){
+        if(!visited[i]) {
+            cout << "NO\n" << 1 << " " << i+1;
+            return 0;
+        }
+    }
+    visited.assign(n,false);
+    cout << endl;
+    bfs(adj2,0,visited);
+    for(int i=0;i<n;i++){
+        if(!visited[i]) {
+            cout << "NO\n" << i+1 << " " << 1;
+            return 0;
+        }
+    }
+    cout << "YES" << "\n"; 
+    return 0;
+}
